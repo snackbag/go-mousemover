a190f4eeb41095382099c54301c37841d5a4c800
JXSnack <jx.snack@gmail.com>
2024-12-16 22:21:22 +0100

    macos implementation

---
+++ mouse_darwin.go
@@ -0,0 +1,31 @@
+// mouse_darwin.go
+//go:build darwin
+// +build darwin
+
+package main
+
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework ApplicationServices
+#include <ApplicationServices/ApplicationServices.h>
+
+int moveMouse(int x, int y) {
+    CGEventRef moveEvent = CGEventCreateMouseEvent(NULL, kCGEventMouseMoved, CGPointMake(x, y), kCGMouseButtonLeft);
+    if (moveEvent == NULL) {
+        return -1;
+    }
+    CGEventPost(kCGHIDEventTap, moveEvent);
+    CFRelease(moveEvent);
+    return 0;
+}
+*/
+import "C"
+import "fmt"
+
+func MoveMouse(x, y int) error {
+	result := C.moveMouse(C.int(x), C.int(y))
+	if result != 0 {
+		return fmt.Errorf("failed to move mouse on macOS")
+	}
+	return nil
+}
